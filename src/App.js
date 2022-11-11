d619677e00513f5c17cbf218243c0a67a61d7950
Mac Mini <macmini@Macs-Mac-mini.local>
2022-11-11 14:59:54 +0600

    mazidulislam

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
 import './App.css';
 
 function App() {
+
+  const [posts, setPosts]=useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      
+      localStorage.setItem("postArray", JSON.stringify(json));
+    })
+  },[])
+
+  const getPostArray =()=>{
+    debugger
+    const postArray = JSON.parse(localStorage.getItem("postArray"));
+    setPosts(postArray);
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <h1>{posts?.length}</h1>
+        <h1>{posts[0]?.userId}</h1>
+        <h2>{posts[0]?.title}</h2>
+
+        <button onClick={getPostArray}>Get From localStorage</button>
+
+     
+      <div className="grid grid-rows-4 grid-flow-col gap-4">
+        
+      {
+        posts?.forEach((post) => {
+          <div>
+            <h1>{post?.title}</h1>
+            <div className='flex-auto'>
+              <h3>{post?.id}</h3>
+              <h3>{post?.userId}</h3>
+            </div>
+            <p>{post?.body}</p>
+
+          </div>
+           })}
+</div>
+
       </header>
     </div>
   );
